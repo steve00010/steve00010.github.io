976a5c0e454f09b28e74a025bc019f5294083647
steve00010 <steve.howie@hotmail.com>
2015-06-30 18:02:56 +0100

    Remake the whole website

---
+++ assets/js/sticky.js
@@ -0,0 +1,28 @@
+﻿/* affix the navbar after scroll below header */
+$('#nav').affix({
+      offset: {
+        top: $('header').height()-$('#nav').height()+55
+      }
+});
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' })
+
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+})
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top+20;
+  $('body,html').animate({scrollTop:posi},700);
+})
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top-50
+    }, 500);
+    return false;
+});
